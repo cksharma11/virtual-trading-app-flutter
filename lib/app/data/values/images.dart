ccd8bd7128785b2ec7ae49f3b2dc7634bcc2d671
Chandan Kumar <cksharma122@gmail.com>
2022-01-09 16:10:11 +0530

    Order basic skeleton page

--- lib/app/data/values/images.dart
+++ lib/app/data/values/images.dart
@@ -2,4 +2,5 @@ class Images {
   Images._privateConstructor();
 
   static const icAppLogo = 'assets/images/ic_app_logo.png';
+  static const stockChartImage = 'assets/images/stocks.png';
 }
--- lib/app/modules/dashboard/orders/views/orders_view.dart
+++ lib/app/modules/dashboard/orders/views/orders_view.dart
@@ -1,10 +1,68 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:starter/app/data/values/images.dart';
+import 'package:starter/app/theme/styles.dart';
+import 'package:starter/widgets/search/search_bar.dart';
 
 class OrdersView extends StatelessWidget {
   const OrdersView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('LOL 2'));
+    return SafeArea(
+      child: Column(
+        children: [
+          Text(
+            'Orders',
+            style: TextStyle(fontSize: 24),
+            textAlign: TextAlign.left,
+          ),
+          DefaultTabController(
+            length: 3,
+            initialIndex: 0,
+            child: TabBar(
+              isScrollable: false,
+              controller: null,
+              tabs: [
+                Tab(
+                  child: Text(
+                    'Open',
+                    style: Styles.tsPrimaryColorRegular14,
+                  ),
+                ),
+                Tab(
+                  child: Text(
+                    'Executed',
+                    style: Styles.tsPrimaryColorRegular14,
+                  ),
+                ),
+                Tab(
+                  child: Text(
+                    'GTT',
+                    style: Styles.tsPrimaryColorRegular14,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SearchBar(),
+          SafeArea(
+            child: Column(
+              children: [
+                Image(image: AssetImage(Images.stockChartImage),),
+                Text(
+                  'No pending orders',
+                  style: Styles.tsPrimaryColorRegular18,
+                ),
+                Text(
+                  'Place your order from your watchlist',
+                  style: Styles.tsPrimaryColorRegular14,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
